1dbe7d56710d14129a2ce082df05cb23094a7891
Thadeu84 <marciothadeu1984@gmail.com>
2023-01-26 20:42:48 -0300

    new file jenkins/values.yaml

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "google_container_node_pool" "primary_preemtible_node" {
   }
 
   node_config {
-    preemptible   = true
+    preemptible  = true
     machine_type = "e2-medium"
 
     labels = {
--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 terraform {
-    required_version = ">= 0.14"
+  required_version = ">= 0.14"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.50.0"
     }
   }
