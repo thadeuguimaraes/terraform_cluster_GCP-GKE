3ddb1db94d9b46ca6d6df9661e01a804da54ef12
Thadeu84 <marciothadeu1984@gmail.com>
2023-01-26 17:59:21 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "google_container_cluster" "primary" {
+  name                     = "my-gke-cluster"
+  location                 = "us-central1"
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  node_locations = [
+    "us-central1-c"
+  ]
+
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+  }
+
+  release_channel {
+    channel = "REGULAR"
+  }
+}
+
+resource "google_container_node_pool" "primary_preemtible_node" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible   = true
+    machine_type = "e2-medium"
+
+    labels = {
+      foo = "bar"
+    }
+  }
+}
